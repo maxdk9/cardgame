09526f6329da58177af6bdbb47bf557af92ae47f
maxdk9 <maxdk9@gmail.com>
2018-12-20 17:53:00 +0200

    cardfiles

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,7 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace DefaultNamespace
 {
     public class CardManager
     {
+        private static CardManager _instance;
+        public List<Card> deck;
         
+
+        public static CardManager getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance=new CardManager();
+                
+            }
+
+            return _instance;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public struct Card
+        {
+            public string Name;
+            public string Sprite;
+                              public int difficulty;
+                                                    public int deadliness;
+
+            public Card(string name, string sprite, int difficulty, int deadliness)
+            {
+                this.Name = name;
+                this.Sprite = sprite;
+                this.difficulty = difficulty;
+                this.deadliness = deadliness;
+            }
+        }
+
+        public CardManager()
+        {
+            deck=new List<Card>();
+        }
     }
+
+    public class CardManagerScr : MonoBehaviour
+    {
+        private void Awake()
+        {
+            CardManager.getInstance().deck.Add(new CardManager.Card(CardNames.argo));
+            
+        }
+    }
+    
 }
---
+++ Assets/Scripts/CardNames.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace DefaultNamespace
+{
+    public class CardNames
+    {
+        public static String skeleton = "skeleton";
+        public static String harpy = "harpy";
+        public static String hydra = "hydra";
+        public static String gorgon = "gorgon";
+        public static String typhon = "typhon";
+        public static String echidna = "echidna";
+        public static String minotaur = "minotaur";
+        public static String cyclops = "cyclops";
+        public static String cerberus = "cerberus";
+        public static String manticore = "manticore";
+        public static String colchiandragon = "colchiandragon";
+        public static String scylla = "scylla";
+        public static String charybdis = "charybdis";
+        public static String ketos = "ketos";
+        public static String khalkotaurus = "khalkotaurus";
+        public static String sirens = "sirens";
+        public static String argo = "argo";
+        public static String swordofpeleus = "swordofpeleus";
+        public static String wingedsandals = "wingedsandals";
+        public static String cornucopia = "cornucopia";
+        public static String goldenfleece = "goldenfleece";
+        public static String pansflute = "pansflute";
+        public static String mirroredshield = "mirroredshield";
+        public static String orpheuslyre = "orpheuslyre";
+        public static String chimera = "chimera";
+        public static String nemeanlion = "nemeanlion";
+        public static String sphinx = "sphinx";
+        public static String helmofhades = "helmofhades";
+        public static String cloakofheracles = "cloakofheracles";
+        public static String poseidontrident = "poseidontrident";
+        public static String ambrosia = "ambrosia";
+        public static String daedaluswing = "daedaluswing";
+        public static String aegisofzeus = "aegisofzeus";
+        public static String apollobow = "apollobow";
+        public static String hera_blessing = "hera_blessing";
+        public static String poseidon_wrath = "poseidon_wrath";
+        public static String hades_wrath = "hades_wrath";
+        public static String zeus_blessing = "zeus_blessing";
+        public static String apollo_blessing = "apollo_blessing";
+    }
+}
